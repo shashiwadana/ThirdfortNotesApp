6fd647e3717441423dabed612c254a6c3d30c3e9
Shashi <shashiwadananirmani14@gmail.com>
2021-01-11 09:51:56 +0530

    addNote

---
+++ src/controllers/addNote.controller.js
@@ -0,0 +1,28 @@
+const {db,conn, spAsync}= require('../../db/index');
+let spAddNote = `Call addNote (?,?,?)`;
+
+const addNote = (req, res) => {
+  
+    let title=req.body.title;
+    let description = req.body.description;
+    let archive_status= req.body.archive_status
+    
+    spAsync(spAddNote, [title,description,archive_status,])
+      .then(result => {
+        res.status(200).json({
+          status: 200,
+          result: result,
+          message: "OK"
+        });
+      })
+      .catch(err => {
+        res.send(500).json({
+          status: 500,
+          result: err,
+          message: "Internal Server Error"
+        });
+      });
+  };
+
+  module.exports.addNote = addNote
+  
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const getAllNotesController = require('../controllers/getAllNotes.controller');
+const addNoteController = require('../controllers/addNote.controller');
 
 router.get('/getAllNotes',(req,res)=>{
     getAllNotesController.getAllNotes(req,res)
 })
-
+router.post('/addNote',(req,res)=>{
+    addNoteController.addNote(req,res)
+})
 
 module.exports=router;
