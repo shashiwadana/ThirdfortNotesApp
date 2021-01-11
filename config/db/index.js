89fe6b778dc3f7facb2936635104582a6c9fab35
Shashi <shashiwadananirmani14@gmail.com>
2021-01-11 09:21:12 +0530

    db configured

---
+++ config/db/index.js
@@ -0,0 +1,40 @@
+const { query } = require('express')
+const mysql = require('mysql')
+const config = require('../config/keys')
+
+const conn = mysql.createPool(config.db)
+
+// Executes Stores Procedures Asynchronously and returns Result
+const spAsync = async (spName,inputs=[])=>{
+    let promise = new Promise((resolve,reject)=>{
+        setTimeout(()=>{conn.getConnection((err,connection)=>{
+            if(err){
+                connection.release();
+                throw err;
+            }
+       
+               connection.query(spName,[],(err,result)=>{
+                if(!err){
+                    resolve(result)
+                    reject('Connection Timed Out')
+                }
+                else{
+                    console.error(err)
+                    resolve(err)
+                }
+            })
+        })
+    },1)
+    })
+
+
+    let result = await promise;
+    return result
+   
+} 
+
+
+
+module.exports.spAsync = spAsync
+module.exports.mysql = mysql
+module.exports.conn = conn
---
+++ config/keys.js
@@ -0,0 +1,13 @@
+module.exports = {
+    db:{
+        user: 'root',
+        password: '',
+        host: 'localhost', 
+        database: 'notes_app_db',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    },
+    
+
+};
